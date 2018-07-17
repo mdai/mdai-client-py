f89d597fb715d58831cb41ab76ad3cbc2652cd24
Tian Xia <tian@md.ai>
2018-07-17 18:09:03 -0400

    refator for hello world notebook

--- mdai/utils.py
+++ mdai/utils.py
@@ -1,19 +1,20 @@
-import random 
-from PIL import Image 
+import random
+from PIL import Image
+
 
 def hex2rgb(h):
     """Convert Hex color encoding to RGB color"""
-    h = h.lstrip('#')
-    return tuple(int(h[i:i+2], 16) for i in (0, 2 ,4))
+    h = h.lstrip("#")
+    return tuple(int(h[i : i + 2], 16) for i in (0, 2, 4))
+
 
 def train_test_split(image_ids, shuffle=True, validation_split=0.1):
-    """ Split image ids into training and validation sets. 
-    """
-    if validation_split < 0.0 or validation_split > 1.0: 
-        return None, None 
+    """Split image ids into training and validation sets."""
+    if validation_split < 0.0 or validation_split > 1.0:
+        return None, None
 
     image_ids_list = list(image_ids)
-    if shuffle: 
+    if shuffle:
         sorted(image_ids_list)
         random.seed(42)
         random.shuffle(image_ids_list)
@@ -22,7 +23,8 @@ def train_test_split(image_ids, shuffle=True, validation_split=0.1):
     image_ids_train = image_ids_list[:split_index]
     image_ids_val = image_ids_list[split_index:]
 
-    print("Num of instances for training set: %d, validation set: %d" 
-          % (len(image_ids_train), len(image_ids_val)))
+    print(
+        "Num of instances for training set: %d, validation set: %d"
+        % (len(image_ids_train), len(image_ids_val))
+    )
     return image_ids_train, image_ids_val
-
