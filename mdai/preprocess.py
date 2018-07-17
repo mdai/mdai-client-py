e9dd7c6dc407a8b462f3e72b0085bd4858b7e1a3
Tian Xia <tian@md.ai>
2018-07-17 15:59:39 -0400

    added hello world example, refactored preprocess

--- mdai/preprocess.py
+++ mdai/preprocess.py
@@ -12,29 +12,84 @@ import logging
 logging.basicConfig(level=logging.INFO)
 _LOGGER = logging.getLogger(__name__)
 
-def get_label_ids(labels, local_only=False):
-    """ Get label ids for labels. """
-    if local_only: 
-        return [label['id']for label in labels if label['type'] == 'local' ]
-    else: 
-        return [label['id'] for label in labels]
+# def get_label_ids(labels, local_only=False):
+#     """ Get label ids for labels. """
+#     if local_only: 
+#         return [label['id']for label in labels if label['type'] == 'local' ]
+#     else: 
+#         return [label['id'] for label in labels]
 
-# TODO: rename 
-def get_label_ids_dict(label_ids, labels): 
-    """Create a dict with label id as key, and 
-       a nested dict of class_id, and class_text as values 
-       e.g., {'L_v8n': {'class_id': 1, 'class_text': 'Lung Opacity'}}
-       where L_v8n is the label id, with a class_id of 1 and class text of 'Lung Opacity'
-    """ 
-    label_ids_dict = {}
-    for i, llid in enumerate(label_ids):
-        for label in labels: 
-            if label['id'] == llid: 
-                class_text = label['name']
+# # TODO: rename 
+# def get_label_ids_dict(label_ids, labels): 
+#     """Create a dict with label id as key, and 
+#        a nested dict of class_id, and class_text as values 
+#        e.g., {'L_v8n': {'class_id': 1, 'class_text': 'Lung Opacity'}}
+#        where L_v8n is the label id, with a class_id of 1 and class text of 'Lung Opacity'
+#     """ 
+#     label_ids_dict = {}
+#     for i, llid in enumerate(label_ids):
+#         for label in labels: 
+#             if label['id'] == llid: 
+#                 class_text = label['name']
                 
-        label_ids_dict[llid] = {'class_id': i+1, 'class_text': class_text}
+#         label_ids_dict[llid] = {'class_id': i+1, 'class_text': class_text}
 
-    return label_ids_dict
+#     return label_ids_dict
+
+class Project(object):
+
+    def __init__(self, data_fp=None, images_fp=None):
+        self.data_fp = None 
+        self.images_fp = None 
+        self.label_groups = [] 
+        self.datasets = []
+
+        if data_fp is not None and images_fp is not None: 
+            self.data_fp = data_fp
+            self.images_fp = images_fp
+
+        with open(self.data_fp, 'r') as f:
+                self.data = json.load(f)
+        
+        for dataset in self.data['datasets']:
+            self.datasets.append(Dataset(dataset, images_fp))
+
+        for label_group in self.data['labelGroups']:
+            self.label_groups.append(LabelGroup(label_group))
+
+    def get_label_groups(self):
+        #print('Available Label Groups are:')
+        #label_groups_df = pd.DataFrame(self.data['labelGroups'])
+        #return label_groups_df
+
+        # for label_group in self.label_groups:
+        #      print('Name: %s, Id: %s' % (label_group.name, label_group.id)) 
+        #      print(label_group.get_labels())
+        return self.label_groups
+
+    def get_label_group_by_id(self, label_group_id): 
+        for label_group in self.label_groups: 
+            if label_group.id == label_group_id: 
+                return label_group
+        return None
+    
+    def get_datasets(self):
+        return self.datasets
+
+    def get_dataset_by_name(self, dataset_name):
+        for dataset in self.datasets: 
+            if dataset.name == dataset_name: 
+                return dataset
+        return None 
+
+class LabelGroup(): 
+    def __init__(self, label_group):
+        self.label_group_data = label_group
+        self.name = self.label_group_data['name']
+        self.id = self.label_group_data['id']
+
+    def get_labels(self): 
+        return [(label['id'], label['name']) for label in self.label_group_data['labels']]
 
 class Dataset(object): 
     """
@@ -50,100 +105,19 @@ class Dataset(object):
     local annottations apply to a part of the image (e.g., ROI bounding box).
     """ 
 
-    label_group_id = -1 
-    project_data_fp = None 
-    project_images_fp = None 
-
-    def __init__(self, project_data_fp=None, project_images_fp=None): 
-        """ project_data_fp is the annotation, 
-            project_images_fp is the DICOM image directory. 
+    def __init__(self, dataset_data, images_fp): 
+        """ images_fp is the DICOM image directory. 
         """
-        if project_data_fp is not None and project_images_fp is not None: 
-            self.project_data_fp = project_data_fp
-            self.project_images_fp = project_images_fp
+        self.dataset_data = dataset_data 
+        self.images_fp = images_fp
 
-            with open(project_data_fp, 'r') as f:
-                self.data = json.load(f)
-        self.annotations = self.data['datasets'][0]['annotations']
-
-    def _is_valid_label_group(self, label_group_id):
-        """Check to see if label group id is valid. """
-        num_label_groups = len(self.data['labelGroups'])
-        if label_group_id >= 0 and label_group_id < num_label_groups:  
-            return True
-        else: 
-            return False
-
-    def set_label_group(self, label_group_id):
-        """Set desired label group. """ 
-        if self._is_valid_label_group(label_group_id): 
-            print('Setting label group to %s, Id: %d' % (self.data['labelGroups'][label_group_id]['name'], label_group_id))
-            self.label_group_id = label_group_id
-            return True 
-        else: 
-            print('Label Group id %d is invalid' % (label_group_id))
-            return False 
-
-    def show_label_groups(self): 
-        """Show avaiable label groups.""" 
-        print('Available Label Groups are:')
-        for i, label_group in enumerate(self.data['labelGroups']):
-            print('Id: %d, Name: %s' % (i, label_group['name'])) 
-
-    def get_labels(self, label_group_id=-1): 
-        """Once label group is selected, get labels for the label group."""
-        if label_group_id == -1 and self.label_group_id != -1: 
-            print('Getting labels for Label Group id: %d' % self.label_group_id)
-            self.labels = self.data['labelGroups'][self.label_group_id]['labels']
-            return self.labels
-
-        # if no label_group_id is specified 
-        if label_group_id == -1: 
-            print('Please specify label group id using set_label_group():')
-            self.show_label_groups() 
-            return None 
-
-        else: 
-            if self._is_valid_label_group(label_group_id): 
-                print('Getting labels for Label Group id: ', label_group_id)
-                self.labels = self.data['labelGroups'][label_group_id]['labels']
-                return self.labels
-            else: 
-                print('Invalid label group id')
-                self.show_label_groups() 
-                return None 
-
-    def show_labels(self): 
-        """Show labels for selected label group."""
-        if self.label_group_id == -1: 
-            print('Need to specify Label Group Id with set_label_group()')
-            self.show_label_groups() 
-        print('Labels:')
-        for label in self.labels: 
-            print("id: %s, name: %s, type: %s" % (label['id'], label['name'], label['type']))
-
-
+        self.id = dataset_data['id']
+        self.name = dataset_data['name']
+        self.annotations = dataset_data['annotations']
+   
     def get_annotations(self, label_ids=None):
-        """
-            Filtered annotations for specified label_ids.
-        """
-        #print("Dataset contains %d annotations." % len(self.annotations))
-
         if label_ids is None: 
             return self.annotations
-
-        if self.label_group_id == -1: 
-            print('Need to specify Label Group Id with set_label_group()')
-            self.show_label_groups() 
-            return None
-
-        self.labels = self.data['labelGroups'][self.label_group_id]['labels']
-
-        print('Filtered annotations for these labels:')
-        for label in self.labels: 
-            if label['id'] in label_ids: 
-                print(label['name'])
-        print('\n')
         
         annotations_filtered = [a for a in self.annotations if a['labelId'] in label_ids]
         print("Filtered Dataset contains %d annotations." % len(annotations_filtered))
@@ -154,7 +128,7 @@ class Dataset(object):
         """ 
         uid = None
         try: 
-            uid = os.path.join(self.project_images_fp, ann['StudyInstanceUID'], 
+            uid = os.path.join(self.images_fp, ann['StudyInstanceUID'], 
                                ann['SeriesInstanceUID'], ann['SOPInstanceUID']) + '.dcm'
         except Exception as error: 
             print('Exception:', error)
@@ -170,14 +144,15 @@ class Dataset(object):
                 image_ids.add(uid)
         return list(image_ids)
 
-    def associate_images_and_annotations(self, image_fps, local_label_ids, ann): 
+    def associate_images_and_annotations(self, ann, label_ids): 
         """Associate image annotations with the corresponding image,
            using a unique file path identifier. 
         """
-        images_and_annotations = {fp: [] for fp in image_fps}
+        image_ids = self.get_image_ids(ann)
+        images_and_annotations = {fp: [] for fp in image_ids}
         for a in ann:
             # only add local annotations with data
-            if a['labelId'] in local_label_ids and a['data'] is not None:
+            if a['labelId'] in label_ids:
                 uid = self._generate_uid(a)
                 images_and_annotations[uid].append(a)
         return images_and_annotations
