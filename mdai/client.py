6b59a514b2e5dcfcb39546e87fa64e387c75480c
Leon Chen <leon@md.ai>
2019-03-11 02:09:26 -0400

    implement annotations import method

--- mdai/client.py
+++ mdai/client.py
@@ -85,6 +85,27 @@ class Client:
             print("No project created. Downloaded annotations only.")
             return None
 
+    def load_model_annotations(self, project_id, dataset_id, model_id, annotations):
+        """Method for loading machine learning model results into project as annotations.
+
+        Arguments:
+            project_id: hash ID of project.
+            dataset_id: hash ID of machine learning model.
+            model_id: hash ID of machine learning model.
+            annotations: list of annotations to load.
+        """
+        manager = AnnotationsImportManager(
+            domain=self.domain,
+            project_id=project_id,
+            dataset_id=dataset_id,
+            model_id=model_id,
+            annotations=annotations,
+            session=self.session,
+            headers=self._create_headers(),
+        )
+        manager.create_job()
+        manager.wait_until_ready()
+
     def _create_headers(self):
         headers = {}
         if self.access_token:
@@ -346,3 +367,138 @@ class ProjectDataManager:
         # fire ready threading.Event
         self._ready.set()
         print("Success: {} data for project {} ready.".format(self.type, self.project_id))
+
+
+class AnnotationsImportManager:
+    """Manager for importing annotations.
+    """
+
+    def __init__(
+        self,
+        domain=None,
+        project_id=None,
+        dataset_id=None,
+        model_id=None,
+        annotations=None,
+        session=None,
+        headers=None,
+    ):
+        if not domain:
+            raise ValueError("domain is not specified.")
+        if not project_id:
+            raise ValueError("project_id is not specified.")
+
+        self.domain = domain
+        self.project_id = project_id
+        self.dataset_id = dataset_id
+        self.model_id = model_id
+        self.annotations = annotations
+        if session and isinstance(session, requests.Session):
+            self.session = session
+        else:
+            self.session = requests.Session()
+        self.headers = headers
+
+        self.job_id = None
+
+        # ready threading event
+        self._ready = threading.Event()
+
+    def create_job(self):
+        """Create annotations import job through MD.ai API.
+        This is an async operation. Status code of 202 indicates successful creation of job.
+        """
+        endpoint = "https://{}/api/data-import/annotations".format(self.domain)
+        params = {
+            "projectHashId": self.project_id,
+            "datasetHashId": self.dataset_id,
+            "modelHashId": self.model_id,
+            "annotations": self.annotations,
+        }
+        r = self.session.post(endpoint, json=params, headers=self.headers)
+        if r.status_code == 202:
+            self.job_id = r.json()["jobId"]
+            msg = (
+                "Importing annotations into project {}, ".format(self.project_id)
+                + "dataset {}, ".format(self.dataset_id)
+                + "model {}...".format(self.model_id)
+            )
+            print(msg.ljust(100))
+            self._check_job_progress()
+        else:
+            if r.status_code == 401:
+                msg = (
+                    "Project, dataset, or model does not exist, "
+                    + "or you do not have sufficient permissions."
+                )
+                print(msg)
+            self._on_job_error()
+
+    def wait_until_ready(self):
+        self._ready.wait()
+
+    def _check_job_progress(self):
+        """Poll for annotations import job progress.
+        """
+        endpoint = "https://{}/api/data-import/annotations/progress".format(self.domain)
+        params = {"projectHashId": self.project_id, "jobId": self.job_id}
+        r = self.session.post(endpoint, json=params, headers=self.headers)
+
+        try:
+            body = r.json()
+            status = body["status"]
+        except (TypeError, KeyError):
+            self._on_job_error()
+            return
+
+        if status == "running":
+            try:
+                progress = int(body["progress"])
+            except (TypeError, ValueError):
+                progress = 0
+            try:
+                time_remaining = int(body["timeRemaining"])
+            except (TypeError, ValueError):
+                time_remaining = 0
+
+            # print formatted progress info
+            if progress > 0 and progress <= 100 and time_remaining > 0:
+                if time_remaining > 45:
+                    time_remaining_fmt = (
+                        arrow.now().shift(seconds=time_remaining).humanize(only_distance=True)
+                    )
+                else:
+                    # arrow humanizes <= 45 to 'in seconds' or 'just now',
+                    # so we will opt to be explicit instead.
+                    time_remaining_fmt = "in {} seconds".format(time_remaining)
+                end_char = "\r" if progress < 100 else "\n"
+                msg = "Annotations import for project {}...{}% (time remaining: {}).".format(
+                    self.project_id, progress, time_remaining_fmt
+                )
+                print(msg.ljust(100), end=end_char, flush=True)
+
+            # run progress check at 1s intervals so long as status == 'running' and progress < 100
+            if progress < 100:
+                t = threading.Timer(1.0, self._check_job_progress)
+                t.start()
+
+            return
+        elif status == "done":
+            self._on_job_done()
+            # fire ready threading.Event
+            self._ready.set()
+        elif status == "error":
+            self._on_job_error()
+
+    def _on_job_done(self):
+        endpoint = "https://{}/api/data-export/annotations/done".format(self.domain)
+        params = {"projectHashId": self.project_id, "jobId": self.job_id}
+        r = self.session.post(endpoint, json=params, headers=self.headers)
+        print("Successfully imported annotations into project {}.".format(self.project_id))
+
+    def _on_data_export_job_error(self):
+        endpoint = "https://{}/api/data-export/annotations/error".format(self.domain)
+        params = {"projectHashId": self.project_id, "jobId": self.job_id}
+        r = self.session.post(endpoint, json=params, headers=self.headers)
+        print("Error importing annotations into project {}.".format(self.project_id))
+
