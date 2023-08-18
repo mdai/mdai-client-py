bf67ec97c78de33c85f7d12ba25edec682350015
wzhlifelover <zhihao@md.ai>
2023-08-18 01:54:14 +0000

    DICOM SR and DICOM SEG import export [sc-5791]

--- mdai/client.py
+++ mdai/client.py
@@ -10,6 +10,8 @@ import urllib3.exceptions
 from retrying import retry
 from tqdm import tqdm
 import arrow
+import pydicom
+from pydicom.filereader import dcmread
 from .preprocess import Project
 from . import __version__
 
@@ -240,6 +242,127 @@ class Client:
             )
 
         return failed_annotations
+
+    def import_SR(self, file_path, json_out=False, project_id="", dataset_id="", label_id=""):
+        """
+        Inputs:
+            `file_path` - File path to the SR (required)
+            `json_out` - Boolean flag to determine if should output to JSON (optional)
+            `project_id` & `dataset_id` & `label_id` - Project information necessary to output SR to annotation note. All must be present if any are present. (optional)
+        Outputs:
+            If `json_out` is `True` then there will be a json file in your cwd called "SR_content".
+
+        Created by Dyllan Hofflich and MD.ai
+        """
+        ds = dcmread(file_path)
+
+        # Get the referenced Dicom Files
+        referenced_dicoms = []
+        for study_seq in ds.CurrentRequestedProcedureEvidenceSequence:
+            referenced_study = {}
+            study_UID = study_seq.StudyInstanceUID
+            referenced_study["Study UID"] = study_UID
+            referenced_dicoms.append(referenced_study)
+
+        content_seq_list = list(ds.ContentSequence)
+
+        content = []
+        self.iterate_content_seq(content, content_seq_list)
+
+        final_content = []
+        for annot in content:
+            annot = list(filter(None, annot))
+            final_content.append(" - ".join(annot))
+
+        if json_out:
+            out_json = {}
+            out_json["Referenced DICOM"] = referenced_dicoms
+            out_json["SR Content"] = final_content
+
+            # Serializing json
+            json_object = json.dumps(out_json, indent=4)
+
+            # Writing to sample.json
+            with open("SR_content.json", "w") as outfile:
+                outfile.write(json_object)
+
+        if project_id or dataset_id or label_id:
+            if not project_id:
+                print('Please add in the "project_id" argument')
+            if not dataset_id:
+                print('Please add in the "dataset_id" argument')
+            if not label_id:
+                print('Please add in the "label_id" argument')
+
+            annotations = []
+            for dicom_dict in referenced_dicoms:
+                study_uid = dicom_dict["Study UID"]
+                note = "\n".join(final_content)
+                annot_dict = {"labelId": label_id, "StudyInstanceUID": study_uid, "note": note}
+                annotations.append(annot_dict)
+
+            self.import_annotations(annotations, project_id, dataset_id)
+
+    def iterate_content_seq(self, content, content_seq_list):
+        for content_seq in content_seq_list:
+            parent_labels = []
+            child_labels = []
+            notes = []
+
+            if "RelationshipType" in content_seq:
+                if content_seq.RelationshipType == "HAS ACQ CONTEXT":
+                    continue
+
+            if content_seq.ValueType == "IMAGE":
+                if "ReferencedSOPSequence" in content_seq:
+                    for ref_seq in content_seq.ReferencedSOPSequence:
+                        if "ReferencedSOPClassUID" in ref_seq:
+                            notes.append(
+                                f"\n   Referenced SOP Class UID = {ref_seq.ReferencedSOPClassUID}"
+                            )
+                        if "ReferencedSOPInstanceUID" in ref_seq:
+                            notes.append(
+                                f"\n   Referenced SOP Instance UID = {ref_seq.ReferencedSOPInstanceUID}"
+                            )
+                        if "ReferencedSegmentNumber" in ref_seq:
+                            notes.append(
+                                f"\n   Referenced Segment Number = {ref_seq.ReferencedSegmentNumber}"
+                            )
+                else:
+                    continue
+
+            if "ConceptNameCodeSequence" in content_seq:
+                if len(content_seq.ConceptNameCodeSequence) > 0:
+                    parent_labels.append(content_seq.ConceptNameCodeSequence[0].CodeMeaning)
+            if "ConceptCodeSequence" in content_seq:
+                if len(content_seq.ConceptCodeSequence) > 0:
+                    child_labels.append(content_seq.ConceptCodeSequence[0].CodeMeaning)
+
+            if "DateTime" in content_seq:
+                notes.append(content_seq.DateTime)
+            if "Date" in content_seq:
+                notes.append(content_seq.Date)
+            if "PersonName" in content_seq:
+                notes.append(str(content_seq.PersonName))
+            if "UID" in content_seq:
+                notes.append(content_seq.UID)
+            if "TextValue" in content_seq:
+                child_labels.append(content_seq.TextValue)
+            if "MeasuredValueSequence" in content_seq:
+                if len(content_seq.MeasuredValueSequence) > 0:
+                    units = (
+                        content_seq.MeasuredValueSequence[0]
+                        .MeasurementUnitsCodeSequence[0]
+                        .CodeValue
+                    )
+                    notes.append(str(content_seq.MeasuredValueSequence[0].NumericValue) + units)
+
+            if "ContentSequence" in content_seq:
+                self.iterate_content_seq(content, list(content_seq.ContentSequence))
+            else:
+                content.append(
+                    [", ".join(parent_labels), ", ".join(child_labels), ", ".join(notes)]
+                )
 
     def _create_headers(self):
         headers = {}
